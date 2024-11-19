7493122df5d21ff0a27f6654edf5ab9d49e17a42
antoniog0v <antoniogvillani@gmail.com>
2024-11-19 00:06:52 -0500

    Implemented functionality for creating accounts, and account info is then stored into DB.

--- src/main/java/dao/StorageUploader.java
+++ src/main/java/dao/StorageUploader.java
@@ -8,10 +8,11 @@ import com.azure.storage.blob.BlobContainerClientBuilder;
 public class StorageUploader {
 
     private BlobContainerClient containerClient;
+    private String connectionKey = ("DefaultEndpointsProtocol=https;AccountName=villanicsc311storage;AccountKey=X6ywaGmPMV9Lv7/DM1be5x7oIFQh8EY7v33xA9jykMgOsPekvKXh/zw407+5JA9KjiCPfEiqD+U8+AStxssyIQ==;EndpointSuffix=core.windows.net");
 
     public StorageUploader( ) {
         this.containerClient = new BlobContainerClientBuilder()
-                .connectionString("DefaultEndpointsProtocol=https;AccountName=villanicsc311storage;AccountKey=X6ywaGmPMV9Lv7/DM1be5x7oIFQh8EY7v33xA9jykMgOsPekvKXh/zw407+5JA9KjiCPfEiqD+U8+AStxssyIQ==;EndpointSuffix=core.windows.net")
+                .connectionString(connectionKey)
                 .containerName("media-files")
                 .buildClient();
     }
